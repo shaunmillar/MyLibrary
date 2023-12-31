2e6b6b6690547cfbd5fee05dddff778a30c37e58
Shaun Millar <shaun.millar@gov.bc.ca>
2023-12-30 20:20:06 -0800

    Commit of change to version

--- src/main/java/my/pkg/name/Greeter.java
+++ src/main/java/my/pkg/name/Greeter.java
@@ -1,7 +1,7 @@
 package my.pkg.name;
 
 public class Greeter {
-  String version = "0.0.1";
+  String version = "0.0.2";
   public String sayHello() {
     return "Hello world library, " + version;
   }
